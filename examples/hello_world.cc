22843efa45856010b37241ea8d1e23b5fe830d8e
alibenD <aliben.develop@gmail.com>
2018-11-21 08:58:41 +0800

    Init project

---
+++ examples/hello_world.cc
@@ -0,0 +1,19 @@
+/**-----------------------------------------------
+  * @Copyright (C) All rights reserved.
+  * @filename: opencv_tutorial
+  * @author: aliben.develop@gmail.com
+  * @created_date: 2018-11-21 08:58:41
+  * @last_modified_date: NO_LAST_MODIFIED_DATE
+  * @description: TODO
+  *-----------------------------------------------*/
+
+//INCLUDE
+#include <opencv_tutorial/print_hello.hh>
+
+//CODE
+
+int main(int argc, char** argv)
+{
+  print_hello();
+  return 0;
+}
---
+++ include/opencv_tutorial/print_hello.hh
@@ -0,0 +1,15 @@
+/**-----------------------------------------------
+  * @Copyright (C) All rights reserved.
+  * @filename: opencv_tutorial
+  * @author: aliben.develop@gmail.com
+  * @created_date: 2018-11-21 08:58:41
+  * @last_modified_date: NO_LAST_MODIFIED_DATE
+  * @description: TODO
+  *-----------------------------------------------*/
+
+//INCLUDE
+#include <iostream>
+
+//CODE
+
+int print_hello();
